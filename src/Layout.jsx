f0fb1707337725fd8c879fb62edccf75b4e1c201
AmanQureshi0111 <amanq7362@gmail.com>
2025-10-01 15:43:04 +0530

    updated router

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import Home from './components/Home/Home.jsx'
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 function Layout(){
     return (
         <>
-        <Header/>
-        <Outlet/>
-        <Footer/>
+        <Header />
+        <Outlet />
+        <Footer />
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,28 +6,43 @@ import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
 import Layout from './Layout.jsx'
 import {
   createBrowserRouter,
-  RouterProvider
+  RouterProvider,
+  createRoutesFromElements,
+  Route
 } from 'react-router-dom';
-const router=createBrowserRouter([
-  {
-    path:'/',
-    element: <Layout/>,
-    children: [
-      {
-        path:"home",
-        element:<Home/>
-      },
-      {
-        path:"about",
-        element:<About/>
-      }
-    ]
-  }
-])
-
+// const router=createBrowserRouter([
+//   {
+//     path:'/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path:"home",
+//         element:<Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       },
+//       {
+//         path:"contact",
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+// ])
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout/>}>
+      <Route path="" element={<Home />}/>
+      <Route path="about" element={<About />}/>
+      <Route path="contact" element={<Contact />}/>
+    </Route>
+  )
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}/>
