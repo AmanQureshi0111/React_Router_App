756198b2b17ffc6f9b6b934f5eb5b34af1cc1fb7
AmanQureshi0111 <amanq7362@gmail.com>
2025-10-01 16:45:42 +0530

    completed all pages

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer/Footer.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
-// import User from './components/User/User.jsx'
+import Github,{githubInfoLoader} from './components/Github/Github.jsx'
 import Layout from './Layout.jsx'
 import {
   createBrowserRouter,
@@ -41,6 +41,12 @@ const router=createBrowserRouter(
       <Route path="" element={<Home />}/>
       <Route path="about" element={<About />}/>
       <Route path="contact" element={<Contact />}/>
+      {/* <Route path="user/:userid" element={< User />} /> */}
+      <Route 
+      path="github" 
+      element={<Github />}
+      loader={githubInfoLoader}
+      />
     </Route>
   )
 )
